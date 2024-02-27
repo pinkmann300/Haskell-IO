efe96d9d9c98e7756298840cdfcba48987a43e7b
SandeepC <sandeepchezhian@gmail.com>
2024-02-27 14:37:47 +0530

    Added solution for q1 in exercises

---
+++ chapter-12/bintree-func.hs
@@ -0,0 +1,12 @@
+-- Define an instance of the Functor class for the following type of binary trees that have data in
+-- their nodes:
+
+data Tree a = Leaf | Node (Tree a) a (Tree a) deriving Show
+-- Datatype definition for binary trees. 
+
+instance Functor Tree where
+    fmap _ Leaf = Leaf
+    fmap g (Node m k o) = Node (fmap g m) (g k) (fmap g o)
+
+
+
