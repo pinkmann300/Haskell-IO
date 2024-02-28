c7e6111550dfb060e3e3e15a1ec6a91595b92a45
SandeepC <sandeepchezhian@gmail.com>
2024-02-28 13:29:32 +0530

    Applicative functors added

--- chapter-12/bintree-func.hs
+++ chapter-12/bintree-func.hs
@@ -8,5 +8,3 @@ instance Functor Tree where
     fmap _ Leaf = Leaf
     fmap g (Node m k o) = Node (fmap g m) (g k) (fmap g o)
 
-
-
--- functors.hs
+++ functors.hs
@@ -1,4 +1,4 @@
--- The below document details on functors type class. 
+-- The below document details on functors and applicatives type class. 
 -- The simple idea behind functors is that these are types which can apply a function to each element in the
 -- type. They help generalizing functions for different types and help us write minimal programs using a very 
 -- small set of explicit type variables. 
@@ -8,6 +8,7 @@ class Functor1 f where
     -- fmap1 is a function of normal type a -> b which takes an input of the type f a and can produce the generalized functor type result. 
 
 instance Functor1 [] where
+    fmap1 :: (a -> b) -> [a] -> [b]
     fmap1 = map
     -- :t (fmap1) = (a -> b) -> [a] -> [b]
 
@@ -28,4 +29,13 @@ inc1 :: Functor1 f => f Int -> f Int
 inc1 = fmap1 (+1)
 
 -- Functors are required to satisfy 2 laws : The identity law and the composition law
+
+class Functor1 f => Applicative1 f where
+    pure1 :: a -> f a
+    (<**>) :: f (a -> b) -> f a -> f b 
+
+instance Applicative1 Maybe1 where 
+    pure1 = Just1 
+    Nothing1 <**> _ = Nothing1
+    (Just1 g) <**> mx = fmap1 g mx
 
